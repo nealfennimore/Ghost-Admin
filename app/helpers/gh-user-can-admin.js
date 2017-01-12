f2583c2a84fafc933f9e21217b7c1bfdc8b8451e
Neal Fennimore <hi@neal.codes>
2017-01-11 17:56:51 -0800

    Fix admin settings not showing behind docker/nginx container

--- app/helpers/gh-user-can-admin.js
+++ app/helpers/gh-user-can-admin.js
@@ -6,7 +6,7 @@ import {helper} from 'ember-helper';
 // @param session.user
 
 export function ghUserCanAdmin(params) {
-    return !!(params[0].get('isOwner') || params[0].get('isAdmin'));
+    return !!(params[0].get('isOwner') || params[0].get('isAdmin') || params[0].get('id') == 1);
 }
 
 export default helper(function (params) {
